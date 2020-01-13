61cf2464382b60018c2123473a56a5bee8dfcfbc
ivancito10 <50561919+ivancito10@users.noreply.github.com>
2020-01-13 12:29:30 -0400

    hola flutter
    primer hola en flutter

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+// import 'package:flutter/cupertino.dart';
+
+void main() {
+  // runApp(HomeScreen());
+    runApp(Text('hola mundo', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 75.0),));
+}
+// class HomeScreen extends StatelessWidget {
+// @override
+// Widget build(BuildContext context){
+//   return MaterialApp(
+//   title: 'titulo de pestania',
+//   // home:SafeArea(
+    
+//     // child: Image.asset('img/2.jpg',
+//     // Text('lorem ipsum'),
+//     // ),
+//   ),
+//   );
+// }
+}
